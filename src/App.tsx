9d3f2565acbed49130468448d5ee36cede6154c7
David Y. Stephenson <david@davidystephenson.com>
2025-05-04 16:59:06 -0400

    Initialize convex

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { ReactNode } from 'react'
+import { ChakraProvider, extendTheme, ThemeConfig } from '@chakra-ui/react'
+
+const config: ThemeConfig = {
+  initialColorMode: 'system'
+}
+const theme = extendTheme({ config })
 
 export default function App (): ReactNode {
   return (
-    <>
+    <ChakraProvider theme={theme}>
       <h1>Hello World</h1>
-    </>
+    </ChakraProvider>
   )
 }
