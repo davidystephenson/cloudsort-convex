b1857ccf472330f8f901ce8c50df1724a04100d7
David Y. Stephenson <david@davidystephenson.com>
2025-06-17 23:03:49 -0400

    Inline rename form

--- src/auth/AuthRenameForm.tsx
+++ src/auth/AuthRenameForm.tsx
@@ -1,60 +1,22 @@
-import { FormEvent, JSX, useState } from 'react'
-import { FormRobe, ImpressedRobe, InputRobe, RedIconButtonRobe } from 'robes'
-import { ButtonGroup, InputGroup, InputRightElement } from '@chakra-ui/react'
-import { MdClose } from 'react-icons/md'
+import { JSX, useState } from 'react'
+import { InlineFormRobe } from 'robes'
 import renameAuthContext from './renameAuthContext'
 
 export default function AuthRenameForm (): JSX.Element {
   const rename = renameAuthContext.use()
   const [name, setName] = useState('')
-  const named = name.length > 0
-  function handleSubmit (event: FormEvent): void {
-    event.preventDefault()
-    void rename.act({ name })
-  }
-  const submit = named && (
-    <ImpressedRobe
-      error={rename.errorMessage}
-      size='xs'
-      type='submit'
-      variant='ghost'
-    >
-      Change username
-    </ImpressedRobe>
-  )
-  function handleChange (event: React.ChangeEvent<HTMLInputElement>): void {
-    setName(event.target.value)
-  }
   function handleCancel (): void {
-    setName('')
     rename.deactivate()
   }
+  function handleSubmit (): void {
+    void rename.act({ name })
+  }
   return (
-    <FormRobe
-      stack={{ alignItems: 'center', direction: 'row', width: '300px' }}
+    <InlineFormRobe
+      value={name}
+      onCancel={handleCancel}
       onSubmit={handleSubmit}
-    >
-      <InputGroup width='100%'>
-        <InputRobe
-          width='100%'
-          placeholder='Change username'
-          value={name}
-          onChange={handleChange}
-          autoFocus
-        />
-        <InputRightElement w='fit-content'>
-          <ButtonGroup isAttached mb='10px'>
-            {submit}
-            <RedIconButtonRobe
-              aria-label='Cancel'
-              icon={<MdClose />}
-              onClick={handleCancel}
-              size='xs'
-              variant='ghost'
-            />
-          </ButtonGroup>
-        </InputRightElement>
-      </InputGroup>
-    </FormRobe>
+      onValueChange={setName}
+    />
   )
 }
