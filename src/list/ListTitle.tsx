5ffcc85f779d01bc42411d7a4de1c7bd34726468
David Y. Stephenson <david@davidystephenson.com>
2025-06-19 21:15:00 -0400

    LInk from list page

--- src/list/ListTitle.tsx
+++ src/list/ListTitle.tsx
@@ -2,14 +2,23 @@ import { JSX } from 'react'
 import { Heading, HStack } from '@chakra-ui/react'
 import ListMenu from './ListMenu'
 import ListLabel from './ListLabel'
+import listContext from './listContext'
+import { ButtonClinkRobe } from 'clink-robe'
 
 export default function ListTitle (): JSX.Element {
+  const list = listContext.use()
+  const userPath = `/user/${list.userId}`
   return (
-    <HStack>
-      <Heading size='lg'>
-        <ListLabel />
-      </Heading>
-      <ListMenu />
+    <HStack justify='space-between'>
+      <HStack>
+        <Heading size='lg'>
+          <ListLabel />
+        </Heading>
+        <ListMenu />
+      </HStack>
+      <ButtonClinkRobe to={userPath}>
+        {list.userName}
+      </ButtonClinkRobe>
     </HStack>
   )
 }
